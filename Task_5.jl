ea825d4e6508a9e51a7ee63e907a4da212940438
Danila Karpov <75839359+DanilaKarpov@users.noreply.github.com>
2020-12-22 13:44:39 +0300

    Update Task_5.jl

--- Task_5.jl
+++ Task_5.jl
@@ -7,13 +7,6 @@ function through_rectangles_into_angle(r,angle::NTuple{2,HorizonSide})
     return num_steps
 end
 
-"""
-    moves!(r,sides,num_steps::Vector{Int})
-
--- перемещает Робота по пути, представленного двумя последовательностями, sides и num_steps 
--- sides - содержит последовательность направлений перемещений
--- num_steps - содержит последовательность чисел шагов в каждом из этих направлений, соответственно; при этом, если длина последовательности sides меньше длины последовательности num_steps, то предполагается, что последовательность sides должна быть продолжена периодически       
-"""
 function moves!(r,sides,num_steps::Vector{Int})
     for (i,n) in enumerate(reverse!(num_steps))
         moves!(r, sides[mod(i-1, length(sides))+1], n) # - это не рекурсия (не вызов функцией самой себя), это вызов другой, ранее определенной функции
